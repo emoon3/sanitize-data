72981aa42809277060e0578d2402e3d6c51d24f4
George Jacobs <george@r2d2>
2025-03-22 22:39:21 -0400

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -24,19 +25,42 @@ func init() {
 }
 
 func main() {
+	lambda.Start(handleRequest)
 }
 
 func handleRequest(ctx context.Context, event json.RawMessage) error {
+	type Bucket struct {
+		Name string `json:"name"`
+	}
+
+	type Object struct {
+		Key       string `json:"key"`
+		Size      int    `json:"size"`
+		Etag      string `json:"etag"`
+		VersionID string `json:"version-id"`
+		Sequencer string `json:"sequencer"`
+	}
+
+	type Detail struct {
+		Version         string `json:"version"`
+		Bucket          Bucket `json:"bucket"`
+		Object          Object `json:"object"`
+		RequestID       string `json:"request-id"`
+		Requester       string `json:"requester"`
+		SourceIPAddress string `json:"source-ip-address"`
+		Reason          string `json:"reason"`
+	}
 
 	type Newfile struct {
-		Detail struct {
-			Bucket struct {
-				Name string `json:"name"`
-			} `json:"bucket"`
-			Object struct {
-				Key string `json:"key"`
-			} `json:"object"`
-		} `json:"detail"`
+		Version    string   `json:"version"`
+		ID         string   `json:"id"`
+		DetailType string   `json:"detail-type"`
+		Source     string   `json:"source"`
+		Account    string   `json:"account"`
+		Time       string   `json:"time"`
+		Region     string   `json:"region"`
+		Resources  []string `json:"resources"`
+		Detail     Detail   `json:"detail"`
 	}
 
 	type Record struct {
@@ -48,22 +72,21 @@ func handleRequest(ctx context.Context, event json.RawMessage) error {
 
 	var record Record
 	var newfile Newfile
-	outputbucket := newfile.Detail.Bucket.Name + "-output"
 
-	err := json.Unmarshal([]byte(event), &newfile)
+	err := json.Unmarshal(event, &newfile)
 	if err != nil {
 		fmt.Printf("Problem loading filename. %s\n", err)
 	}
 
-	fileInput := &s3.GetObjectInput{
+	file, err := client.GetObject(ctx, &s3.GetObjectInput{
 		Bucket: aws.String(newfile.Detail.Bucket.Name),
 		Key:    aws.String(newfile.Detail.Object.Key),
+	})
+	if err != nil {
+		fmt.Printf("JSON parse failed. %s %s %s", err, newfile.Detail.Object.Key, newfile.Detail.Bucket.Name)
 	}
 
-	file, err := client.GetObject(ctx, fileInput)
-	if err != nil {
-		fmt.Printf("JSON failed to be parsed. %s\n", err)
-	}
+	fmt.Println(file)
 
 	// capture all bytes from upload
 	body, err := io.ReadAll(file.Body)
@@ -72,7 +95,7 @@ func handleRequest(ctx context.Context, event json.RawMessage) error {
 	}
 	err = json.Unmarshal([]byte(body), &record)
 	if err != nil {
-		fmt.Printf("JSON failed to be parsed. %s\n", err)
+		fmt.Printf("Error loading file. %s\n", err)
 	}
 
 	firstname := []rune(record.Firstname)
@@ -93,8 +116,11 @@ func handleRequest(ctx context.Context, event json.RawMessage) error {
 
 	jsonOutput, err := json.MarshalIndent(record, "", "	")
 	if err != nil {
-		fmt.Printf("JSON failed to be parsed. %s\n", err)
+		fmt.Printf("JSON failed to be encoded. %s\n", err)
 	}
+
+	outputbucket := fmt.Sprintf("%s-output", newfile.Detail.Bucket.Name)
+	log.Println(outputbucket)
 
 	_, err = client.PutObject(ctx, &s3.PutObjectInput{
 		Bucket: aws.String(outputbucket),
@@ -104,5 +130,14 @@ func handleRequest(ctx context.Context, event json.RawMessage) error {
 	if err != nil {
 		fmt.Printf("Error writing file: %s\n", err)
 	}
+
+	_, err = client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(newfile.Detail.Bucket.Name),
+		Key:    aws.String(newfile.Detail.Object.Key),
+	})
+	if err != nil {
+		fmt.Printf("Unredacted file could not be deleted. %s\n", err)
+	}
+
 	return nil
 }
